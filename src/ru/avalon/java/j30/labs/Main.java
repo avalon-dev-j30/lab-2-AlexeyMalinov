1e9b73ab59428495f47e66d10a61f6554adfb352
AlexeyMalinov <alexeymalinov@yandex.ru>
2019-05-08 14:44:28 +0300

    Completed Lab2

--- src/ru/avalon/java/j30/labs/Main.java
+++ src/ru/avalon/java/j30/labs/Main.java
@@ -1,6 +1,10 @@
 package ru.avalon.java.j30.labs;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Properties;
@@ -8,9 +12,10 @@ import java.util.Properties;
 /**
  * Лабораторная работа №3
  * <p>
- * Курс: "DEV-OCPJP. Подготовка к сдаче сертификационных экзаменов серии Oracle Certified Professional Java Programmer"
+ * Курс: "DEV-OCPJP. Подготовка к сдаче сертификационных экзаменов серии Oracle
+ * Certified Professional Java Programmer"
  * <p>
- * Тема: "JDBC - Java Database Connectivity" 
+ * Тема: "JDBC - Java Database Connectivity"
  *
  * @author Daniel Alpatov <danial.alpatov@gmail.com>
  */
@@ -18,13 +23,11 @@ public class Main {
 
     /**
      * Точка входа в приложение
-     * 
+     *
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException {
-        /*
-         * TODO #01 Подключите к проекту все библиотеки, необходимые для соединения с СУБД.
-         */
+    public static void main(String[] args) throws SQLException, IOException {
+
         try (Connection connection = getConnection()) {
             ProductCode code = new ProductCode("MO", 'N', "Movies");
             code.save(connection);
@@ -34,56 +37,52 @@ public class Main {
             code.save(connection);
             printAllCodes(connection);
         }
-        /*
-         * TODO #14 Средствами отладчика проверьте корректность работы программы
-         */
     }
+
     /**
      * Выводит в кодсоль все коды товаров
-     * 
+     *
      * @param connection действительное соединение с базой данных
-     * @throws SQLException 
-     */    
+     * @throws SQLException
+     */
     private static void printAllCodes(Connection connection) throws SQLException {
         Collection<ProductCode> codes = ProductCode.all(connection);
         for (ProductCode code : codes) {
             System.out.println(code);
         }
     }
+
     /**
      * Возвращает URL, описывающий месторасположение базы данных
-     * 
+     *
      * @return URL в виде объекта класса {@link String}
      */
-    private static String getUrl() {
-        /*
-         * TODO #02 Реализуйте метод getUrl
-         */
-        throw new UnsupportedOperationException("Not implemented yet!");
+    private static String getUrl() throws IOException {
+        return getProperties().getProperty("url");
     }
+
     /**
      * Возвращает параметры соединения
-     * 
-     * @return Объект класса {@link Properties}, содержащий параметры user и 
+     *
+     * @return Объект класса {@link Properties}, содержащий параметры user и
      * password
      */
-    private static Properties getProperties() {
-        /*
-         * TODO #03 Реализуйте метод getProperties
-         */
-        throw new UnsupportedOperationException("Not implemented yet!");
+    private static Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        try (InputStream stream = ClassLoader.getSystemResourceAsStream("resources/db.properties")) {
+            properties.load(stream);
+        }
+        return properties;
     }
+
     /**
      * Возвращает соединение с базой данных Sample
-     * 
+     *
      * @return объект типа {@link Connection}
-     * @throws SQLException 
+     * @throws SQLException
      */
-    private static Connection getConnection() throws SQLException {
-        /*
-         * TODO #04 Реализуйте метод getConnection
-         */
-        throw new UnsupportedOperationException("Not implemented yet!");
+    private static Connection getConnection() throws SQLException, IOException {
+        return DriverManager.getConnection(getUrl(), getProperties());
     }
-    
+
 }
